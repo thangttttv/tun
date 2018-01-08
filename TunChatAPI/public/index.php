5c4a4b71854dc23f61761a3d1015629e43aca5eb
thangttttv <thang24011983@gmail.com>
2018-01-08 14:26:20 +0700

    no message

--- TunChatAPI/public/index.php
+++ TunChatAPI/public/index.php
@@ -18,6 +18,10 @@
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 require __DIR__.'/../bootstrap/autoload.php';
 
 /*
