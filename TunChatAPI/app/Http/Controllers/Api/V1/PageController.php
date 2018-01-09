9ef56e21dab1b278296112c825568c0fb1b94602
thangttttv <thang24011983@gmail.com>
2018-01-10 00:12:09 +0700

    no message

--- TunChatAPI/app/Http/Controllers/Api/V1/PageController.php
+++ TunChatAPI/app/Http/Controllers/Api/V1/PageController.php
@@ -42,9 +42,9 @@ class PageController extends Controller
 			'app_secret' => config('services.facebook.client_secret'),
 		]);
 		$pages   = $fb->get('/me/accounts', $fbToken)->getBody();
-		//dd(\GuzzleHttp\json_decode($pages)->data);
-		return \GuzzleHttp\json_decode($pages)->data;
+		//return \GuzzleHttp\json_decode($pages)->data;
 		//return Status::ok()->response();
+		return response()->json($pages->data, 200);
 	}
 
 	//subscribed page
